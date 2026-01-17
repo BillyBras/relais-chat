22de15de7da4b924074a66c34f0d70a59e3482e3
BillyBras <jeremybillybras@gmail.com>
2026-01-17 02:36:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,20 @@ const http = require('http');
 const httpProxy = require('http-proxy');
 
 const proxy = httpProxy.createProxyServer({});
-// Ton adresse Localtonet active
-const PC_URL = 'https://cedwospxv.localto.net'; 
+const PC_URL = 'https://cedwospxv.localto.net';
 
 const server = http.createServer((req, res) => {
-    proxy.web(req, res, { target: PC_URL, changeOrigin: true }, (e) => {
+    req.headers['localtonet-skip-warning'] = 'true';
+
+    proxy.web(req, res, { 
+        target: PC_URL, 
+        changeOrigin: true,
+        headers: { 'localtonet-skip-warning': 'true' }
+    }, (e) => {
         res.writeHead(503, { 'Content-Type': 'text/html; charset=utf-8' });
-        res.end('<h1 style="text-align:center; font-family:sans-serif; margin-top:50px;">🛠️ Serveur en maintenance</h1>');
+        res.end('<h1 style="text-align:center;">🛠️ Serveur en maintenance</h1>');
     });
 });
 
 const PORT = process.env.PORT || 8080;
-server.listen(PORT, () => console.log(`Relais actif sur le port ${PORT}`));
+server.listen(PORT, () => console.log(`Relais bypass actif sur le port ${PORT}`));
